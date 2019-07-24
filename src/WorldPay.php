945bacc79013dfacd4bd5ff066b1977040b936f9
Pablo de la Peña <p@shedcollective.org>
2019-07-24 17:27:26 +0100

    Supporting Payment interface

--- src/WorldPay.php
+++ src/WorldPay.php
@@ -13,6 +13,7 @@
 namespace Nails\Invoice\Driver\Payment;
 
 use Nails\Invoice\Driver\PaymentBase;
+use Nails\Invoice\Factory\ScaResponse;
 
 /**
  * Class WorldPay
@@ -57,12 +58,104 @@ class WorldPay extends PaymentBase
      */
     public function getPaymentFields()
     {
-        return [
-            [
-                'key'      => 'token',
-                'label'    => 'Card Details (WP)',
-                'required' => true,
-            ],
-        ];
+        return [];
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns any assets to load during checkout
+     *
+     * @return array
+     */
+    public function getCheckoutAssets(): array
+    {
+        return [];
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Initiate a payment
+     *
+     * @param integer   $iAmount      The payment amount
+     * @param string    $sCurrency    The payment currency
+     * @param \stdClass $oData        An array of driver data
+     * @param \stdClass $oCustomData  The custom data object
+     * @param string    $sDescription The charge description
+     * @param \stdClass $oPayment     The payment object
+     * @param \stdClass $oInvoice     The invoice object
+     * @param string    $sSuccessUrl  The URL to go to after successful payment
+     * @param string    $sFailUrl     The URL to go to after failed payment
+     * @param string    $sContinueUrl The URL to go to after payment is completed
+     *
+     * @return \Nails\Invoice\Factory\ChargeResponse
+     */
+    public function charge(
+        $iAmount,
+        $sCurrency,
+        $oData,
+        $oCustomData,
+        $sDescription,
+        $oPayment,
+        $oInvoice,
+        $sSuccessUrl,
+        $sFailUrl,
+        $sContinueUrl
+    ) {
+        //  @todo (Pablo - 2019-07-24) - Implement this method
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Handles any SCA requests
+     *
+     * @param ScaResponse $oScaResponse The SCA Response object
+     * @param array       $aData        Any saved SCA data
+     * @param string      $sSuccessUrl  The URL to redirect to after authorisation
+     *
+     * @return ScaResponse
+     */
+    public function sca(ScaResponse $oScaResponse, array $aData, string $sSuccessUrl): ScaResponse
+    {
+        //  @todo (Pablo - 2019-07-24) - Implement this method
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Complete the payment
+     *
+     * @param \stdClass $oPayment  The Payment object
+     * @param \stdClass $oInvoice  The Invoice object
+     * @param array     $aGetVars  Any $_GET variables passed from the redirect flow
+     * @param array     $aPostVars Any $_POST variables passed from the redirect flow
+     *
+     * @return \Nails\Invoice\Factory\CompleteResponse
+     */
+    public function complete($oPayment, $oInvoice, $aGetVars, $aPostVars)
+    {
+        //  @todo (Pablo - 2019-07-24) - Implement this method
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Issue a refund for a payment
+     *
+     * @param string    $sTxnId      The transaction's ID
+     * @param integer   $iAmount     The amount to refund
+     * @param string    $sCurrency   The currency in which to refund
+     * @param \stdClass $oCustomData The custom data object
+     * @param string    $sReason     The refund's reason
+     * @param \stdClass $oPayment    The payment object
+     * @param \stdClass $oInvoice    The invoice object
+     *
+     * @return \Nails\Invoice\Factory\RefundResponse
+     */
+    public function refund($sTxnId, $iAmount, $sCurrency, $oCustomData, $sReason, $oPayment, $oInvoice)
+    {
+        //  @todo (Pablo - 2019-07-24) - Implement this method
     }
 }
