eda73aaabd508379adccb90bd921cf66bb06a7e0
Pablo de la Peña <p@shedcollective.org>
2019-09-10 20:55:46 +0100

    Updating typehints

--- src/WorldPay.php
+++ src/WorldPay.php
@@ -103,16 +103,16 @@ class WorldPay extends PaymentBase
     /**
      * Initiate a payment
      *
-     * @param int                  $iAmount      The payment amount
-     * @param Currency             $oCurrency    The payment currency
-     * @param stdClass             $oData        An array of driver data
-     * @param stdClass             $oPaymentData The payment data object
-     * @param string               $sDescription The charge description
-     * @param Resource\Payment     $oPayment     The payment object
-     * @param Resource\Invoice     $oInvoice     The invoice object
-     * @param string               $sSuccessUrl  The URL to go to after successful payment
-     * @param string               $sErrorUrl    The URL to go to after failed payment
-     * @param Resource\Source|null $oSource      The saved payment source to use
+     * @param int                           $iAmount      The payment amount
+     * @param Currency                      $oCurrency    The payment currency
+     * @param stdClass                      $oData        An array of driver data
+     * @param Resource\Invoice\Data\Payment $oPaymentData The payment data object
+     * @param string                        $sDescription The charge description
+     * @param Resource\Payment              $oPayment     The payment object
+     * @param Resource\Invoice              $oInvoice     The invoice object
+     * @param string                        $sSuccessUrl  The URL to go to after successful payment
+     * @param string                        $sErrorUrl    The URL to go to after failed payment
+     * @param Resource\Source|null          $oSource      The saved payment source to use
      *
      * @return ChargeResponse
      */
@@ -120,7 +120,7 @@ class WorldPay extends PaymentBase
         int $iAmount,
         Currency $oCurrency,
         stdClass $oData,
-        stdClass $oPaymentData,
+        Resource\Invoice\Data\Payment $oPaymentData,
         string $sDescription,
         Resource\Payment $oPayment,
         Resource\Invoice $oInvoice,
@@ -176,13 +176,13 @@ class WorldPay extends PaymentBase
     /**
      * Issue a refund for a payment
      *
-     * @param string           $sTransactionId The transaction's ID
-     * @param int              $iAmount        The amount to refund
-     * @param Currency         $oCurrency      The currency in which to refund
-     * @param stdClass         $oPaymentData   The payment data object
-     * @param string           $sReason        The refund's reason
-     * @param Resource\Payment $oPayment       The payment object
-     * @param Resource\Invoice $oInvoice       The invoice object
+     * @param string                        $sTransactionId The transaction's ID
+     * @param int                           $iAmount        The amount to refund
+     * @param Currency                      $oCurrency      The currency in which to refund
+     * @param Resource\Invoice\Data\Payment $oPaymentData   The payment data object
+     * @param string                        $sReason        The refund's reason
+     * @param Resource\Payment              $oPayment       The payment object
+     * @param Resource\Invoice              $oInvoice       The invoice object
      *
      * @return RefundResponse
      */
@@ -190,7 +190,7 @@ class WorldPay extends PaymentBase
         string $sTransactionId,
         int $iAmount,
         Currency $oCurrency,
-        stdClass $oPaymentData,
+        Resource\Invoice\Data\Payment $oPaymentData,
         string $sReason,
         Resource\Payment $oPayment,
         Resource\Invoice $oInvoice
