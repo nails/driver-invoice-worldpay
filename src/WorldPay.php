54ebdefb50fca3ec93a73ca9b58e46a0a1718bff
Pablo de la Peña <p@shedcollective.org>
2021-04-13 16:08:04 +0100

    Fix incompatability with updated interface

--- src/WorldPay.php
+++ src/WorldPay.php
@@ -979,20 +979,20 @@ class WorldPay extends PaymentBase
     /**
      * Handles any SCA requests
      *
-     * @param ScaResponse $oScaResponse The SCA Response object
-     * @param array       $aData        Any saved SCA data
-     * @param string      $sSuccessUrl  The URL to redirect to after authorisation
+     * @param ScaResponse               $oScaResponse The SCA Response object
+     * @param Resource\Payment\Data\Sca $oData        Any saved SCA data
+     * @param string                    $sSuccessUrl  The URL to redirect to after authorisation
      *
      * @return ScaResponse
      */
-    public function sca(ScaResponse $oScaResponse, array $aData, string $sSuccessUrl): ScaResponse
+    public function sca(ScaResponse $oScaResponse, Resource\Payment\Data\Sca $oData, string $sSuccessUrl): ScaResponse
     {
         /** @var Input $oInput */
         $oInput = Factory::service('Input');
 
         try {
 
-            $oScaData = Sca\Data::buildFromDataArray($aData);
+            $oScaData = Sca\Data::buildFromPaymentData($oData);
 
             if (empty($oInput->post())) {
                 $this->scaInitialPayment($oScaResponse, $oScaData);
