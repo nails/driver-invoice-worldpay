3d4b7ed320cd399a7d51c383e8c20cf60bfe4f73
Pablo de la Peña <p@shedcollective.org>
2021-07-19 12:51:30 +0100

    Add Constants.php

---
+++ src/WorldPay/Constants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nails\Invoice\Driver\Payment\WorldPay;
+
+/**
+ * Class Constants
+ *
+ * @package Nails\Invoice\Driver\Payment\WorldPay
+ */
+class Constants
+{
+    const MODULE_SLUG = 'nails/driver-invoice-worldpay';
+}
